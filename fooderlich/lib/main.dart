006b6f5f7d1b9df6211f8e3579a3c6a6d5f9da27
o0oi1i <o0oi1i@icloud.com>
2021-06-08 18:00:16 +0800

    Initial fooderlich

---
+++ fooderlich/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fooderlich',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Fooderlich')),
+        body: const Center(child: Text('Let\'s get cooking 🍺')),
+      ),
+    );
+  }
+}
