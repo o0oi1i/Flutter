f73ce2e91e5e450eb8312e6dc29c51e6f44a2ff7
o0oi1i <o0oi1i@icloud.com>
2022-01-27 17:58:14 +0800

    Update main.dart

--- Demos/flutter_application_demos/lib/main.dart
+++ Demos/flutter_application_demos/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import '1.hello.dart';
 import '2.skeleton.dart';
+import '3.container.dart';
+import '4.text.dart';
 
 void main() {
-  runApp(Skeleton());
+  runApp(Texts());
 }
