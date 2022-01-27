87fa185ffff08de6ccbe7b954bcaea009b1c3022
o0oi1i <o0oi1i@icloud.com>
2022-01-27 17:32:52 +0800

    Create 2.skeleton.dart

---
+++ Demos/flutter_application_demos/lib/2.skeleton.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Skeleton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Skeleton'),
+        ),
+        body: Body(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Flutter Skeleton',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 36.0,
+          color: Colors.deepPurple.shade300,
+        ),
+      ),
+    );
+  }
+}
