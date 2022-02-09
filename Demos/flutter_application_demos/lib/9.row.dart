86c5ef73dfad01488521892b998ab4de33d75bad
o0oi1i <o0oi1i@icloud.com>
2022-02-09 11:04:13 +0800

    Create 9.row.dart

---
+++ Demos/flutter_application_demos/lib/9.row.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Rows extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Padding'),
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      width: 360.0,
+      color: Colors.pink,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconContainer(Icons.search, color: Colors.blue),
+          IconContainer(Icons.home, color: Colors.orange),
+          IconContainer(Icons.select_all, color: Colors.red),
+        ],
+      ),
+    );
+  }
+}
+
+class IconContainer extends StatelessWidget {
+  IconData icon;
+  Color color;
+
+  IconContainer(this.icon, {this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(
+          this.icon,
+          size: 32.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
