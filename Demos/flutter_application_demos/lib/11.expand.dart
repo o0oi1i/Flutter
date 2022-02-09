f7d873a26db32d9dfc0916aca9f8b60d33ae4f09
o0oi1i <o0oi1i@icloud.com>
2022-02-09 11:46:04 +0800

    Create 11.expand.dart

---
+++ Demos/flutter_application_demos/lib/11.expand.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Expand extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expands'),
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: IconContainer(Icons.search, color: Colors.blue),
+        ),
+        Expanded(
+          flex: 3,
+          child: IconContainer(Icons.home, color: Colors.orange),
+        ),
+        Expanded(
+          flex: 5,
+          child: IconContainer(Icons.select_all, color: Colors.red),
+        ),
+      ],
+    );
+  }
+}
+
+class IconContainer extends StatelessWidget {
+  IconData icon;
+  Color color;
+
+  IconContainer(this.icon, {this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(
+          this.icon,
+          size: 32.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
