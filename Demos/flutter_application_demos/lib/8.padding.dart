e5d918e189d3f4afb62e1f9c5163f40e98341537
o0oi1i <o0oi1i@icloud.com>
2022-02-09 10:42:06 +0800

    Create 8.padding.dart

---
+++ Demos/flutter_application_demos/lib/8.padding.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '6.List/data/listData.dart';
+
+class Paddings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Padding'),
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 60, 30, 20),
+        child: Image.network(
+          'https://www.itying.com/images/flutter/1.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
