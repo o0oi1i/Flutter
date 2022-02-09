e836ce31eae08b58a80f9555ba2246bb3f1ec896
o0oi1i <o0oi1i@icloud.com>
2022-02-09 11:32:57 +0800

    Update 10.column.dart

--- Demos/flutter_application_demos/lib/10.column.dart
+++ Demos/flutter_application_demos/lib/10.column.dart
@@ -17,69 +17,42 @@ class Columns extends StatelessWidget {
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                height: 180,
-                color: Colors.deepPurple,
-                child: Center(
-                  child: Text(
-                    'Hello',
-                    style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
+    return Container(
+      height: 400.0,
+      width: 300.0,
+      color: Colors.pink,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconContainer(Icons.search, color: Colors.blue),
+          IconContainer(Icons.home, color: Colors.orange),
+          IconContainer(Icons.select_all, color: Colors.red),
+        ],
+      ),
+    );
+  }
+}
+
+class IconContainer extends StatelessWidget {
+  IconData icon;
+  Color color;
+
+  IconContainer(this.icon, {this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(
+          this.icon,
+          size: 32.0,
+          color: Colors.white,
         ),
-        SizedBox(height: 10),
-        Row(
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: Container(
-                height: 180,
-                child: Image.network(
-                  "https://www.itying.com/images/flutter/2.png",
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              flex: 3,
-              child: Container(
-                height: 180,
-                child: ListView(
-                  children: <Widget>[
-                    Container(
-                      height: 85,
-                      child: Image.network(
-                        "https://www.itying.com/images/flutter/3.png",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 85,
-                      child: Image.network(
-                        "https://www.itying.com/images/flutter/4.png",
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        )
-      ],
+      ),
     );
   }
 }
