bc0fd64ba7dc1454dc2a2e72920502e0c50118ac
o0oi1i <o0oi1i@icloud.com>
2022-02-12 15:34:36 +0800

    Update main.dart

--- Demos/flutter_application_demos/lib/main.dart
+++ Demos/flutter_application_demos/lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
-import '1.hello.dart';
-import '2.skeleton.dart';
-import '3.container.dart';
-import '4.text.dart';
-import '5.image.dart';
-import '6.list/6.list.dart';
-import '6.list/6.list1.dart';
-import '6.List/6.list2.dart';
-import '6.List/6.list3.dart';
-import '6.List/6.list4.dart';
-import '7.grid.dart';
+// import '1.hello.dart';
+// import '2.skeleton.dart';
+// import '3.container.dart';
+// import '4.text.dart';
+// import '5.image.dart';
+// import '6.list/6.list.dart';
+// import '6.list/6.list1.dart';
+// import '6.List/6.list2.dart';
+// import '6.List/6.list3.dart';
+// import '6.List/6.list4.dart';
+// import '7.grid.dart';
+// import '8.padding.dart';
+// import '9.row.dart';
+// import '10.column.dart';
+// import '11.expand.dart';
+// import '12.layout.dart';
+// import '13.stack.dart';
+// import '14.card.dart';
+import '15.wrap.dart';
 
 void main() {
-  runApp(Grid());
+  runApp(Wraps());
 }
