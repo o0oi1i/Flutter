fd9bcc3270557fbff31794e857c1d68ce6fd3065
o0oi1i <o0oi1i@icloud.com>
2022-03-31 22:36:14 +0800

    Create tabs.dart

---
+++ EC/lib/architecture/tabs.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../screens/card1.dart';
+// import '../screens/card2.dart';
+// import '../screens/card3.dart';
+
+import '../screens/sidebar_screen.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({Key key}) : super(key: key);
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    Card1(),
+    SidebarScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.find_in_page),
+            label: '发现',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: '分类',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.grade),
+            label: '成就',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: '学习',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '我的',
+          ),
+        ],
+      ),
+    );
+  }
+}
