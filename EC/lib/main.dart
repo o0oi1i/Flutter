983585a42a90dbca610010fd9ec4e46c5d76ce3d
o0oi1i <o0oi1i@icloud.com>
2022-03-31 22:36:21 +0800

    Update main.dart

--- EC/lib/main.dart
+++ EC/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mine/architecture/home.dart';
 
 import 'themes/ec_theme.dart';
+import 'routers/router.dart';
 
 void main() {
   runApp(EC());
@@ -14,9 +14,10 @@ class EC extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = ECTheme.dark();
     return MaterialApp(
-      theme: theme,
       title: 'EC',
-      home: Home(),
+      theme: theme,
+      initialRoute: '/',
+      onGenerateRoute: onGenerateRoute,
     );
   }
 }
